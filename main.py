08b09f0d76ec3645fdea7b4b3490e3ce68f169e8
USER - PC <xgcyjsxgcgs@gmail.com>
2024-02-17 11:48:56 +0200

    1.0

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+url = "https://kups.club/"
+
+
+session = requests.Session()
+header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
+
+response = session.get(url, headers=header)
+soup = BeautifulSoup(response.text, "lxml")
+
+all_product = soup.find("div", class_="row mt-4")
+
+products = all_product.find_all("div", class_="col-lg-4 col-md-4 col-sm-6 portfolio-item ")
+print(all_product)
+
+
